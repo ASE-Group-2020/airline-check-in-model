232265e82d2b995e5e6d36c524062802acde3ee5
eddie-LA <ee21@hw.ac.uk>
2020-02-10 15:44:06 +0000

    Implemented: addFlightsFromFile() HashMap fields for Flights and Customer Added exception handling in addFlightsFromFile()

--- AirportCheckin/src/airlineinterface/Flight.java
+++ AirportCheckin/src/airlineinterface/Flight.java
@@ -3,7 +3,7 @@ package airlineinterface;
 public class Flight {
 	
 	// Throw exception if invalid values
-	public Flight(String flightRef, String carrier, int maxPassengers, float maxWeight, float maxVolume) {}
+	public Flight(String departure, String destination, String flightRef, String carrier, int maxPassengers, float maxWeight, float maxVolume) {}
 
 	public void addCustomer(Customer c) {}
 
--- AirportCheckin/src/airlineinterface/Master.java
+++ AirportCheckin/src/airlineinterface/Master.java
@@ -1,9 +1,13 @@
 package airlineinterface;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class Master {
-
-	public static void main(String[] args) {
-	}
+	private HashMap<String, Flight> allFlights;
+	private HashMap<String, Flight> allCustomers;
 	
 	public Master() {}
 	
@@ -16,13 +20,43 @@ public class Master {
 	
 	// Called during startup (reading from file) and with GUI
 	// Needs exception
-	public void checkIn(Customer c, float weight, float volume) {}
+	public void checkIn(Customer c, float weight, float volume) {
+		
+	}
 	
-	public void addFlightsFromFile(String filePath) {}
+	public void addFlightsFromFile(String filePath) {
+		try { //open input stream 
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+        String line = "";
+        while( (line = reader.readLine()) != null ) {
+            String [] flightDetails = line.trim().split(",");
+            Flight currFlight = 
+            		new Flight(flightDetails[0], flightDetails[1], flightDetails[2], flightDetails[3], 
+            				Integer.parseInt(flightDetails[4]), Float.parseFloat(flightDetails[5]), Float.parseFloat(flightDetails[6]));
+            allFlights.put(flightDetails[2],currFlight);
+        }
+        
+		reader.close(); //close reader
+		}
+		catch(Exception e) {
+			if (e instanceof FileNotFoundException)
+				System.out.println("Error: File not found.");
+			else if (e instanceof IOException)
+				System.out.println("Error: I/O error.");
+			else {
+				System.out.println("General error! Give the following information to the devs...");
+				e.printStackTrace();
+			}
+		}
+	}
 	
 	public void addCustomersFromFile(String filePath) {}
 	
 	// Output info for each flight somehow
 	public void display() {}
+	
+	public static void main(String[] args) {
+	}
 
 }
