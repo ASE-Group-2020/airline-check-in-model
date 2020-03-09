8cea0d5061bf391c35b4534596a48def4b78d5c8
Alex <47084301+aswift8@users.noreply.github.com>
2020-03-09 12:25:36 +0000

    Initial WaitingQueue basics
    Added following methods:
    - Constructor
    - run (from Runnable)
    - addCustomersToList (load from file in Simulation, then pass here)
    - hasNext (true/false)
    - getNext (pops and returns next Customer, null if list is empty)
    
    Need to make methods thread-safe where required

--- AirportCheckin/src/airlineinterface/WaitingQueue.java
+++ AirportCheckin/src/airlineinterface/WaitingQueue.java
@@ -1,5 +1,41 @@
 package airlineinterface;
 
-public class WaitingQueue {
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class WaitingQueue implements Runnable {
+
+	Queue<Customer> waiting;
+	List<Customer> notArrived;
+
+	public WaitingQueue() {
+		waiting = new LinkedList<Customer>();
+		notArrived = new LinkedList<Customer>();
+	}
+
+	@Override
+	public void run() {
+		// TODO: simulate adding people from list to queue as they arrive
+	}
+	
+	// Adds every given customer to the not-arrived list, then shuffles
+	public void addCustomersToList(List<Customer> customers) {
+		notArrived.addAll(customers);
+		Collections.shuffle(notArrived);
+	}
+	
+	// True if list has elements, false otherwise
+	// TODO: make thread-safe
+	public boolean hasNext() {
+		return !waiting.isEmpty();
+	}
+	
+	// Pop and return customer - null if empty
+	// TODO: make thread-safe
+	public Customer getNext() {
+		return waiting.poll();
+	}
 
 }
