4ea98b5e8fad974224453e70cd14964a8b52ca4e
DigitalTether <niko.tether@sisu-fm.co.uk>
2020-03-16 15:26:27 +0000

    Log class update (currently untested)

--- AirportCheckin/src/airlineinterface/Logger.java
+++ AirportCheckin/src/airlineinterface/Logger.java
@@ -1,5 +1,9 @@
 package airlineinterface;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Logger {
 
 	// Statics
@@ -10,6 +14,9 @@ public class Logger {
 	}
 
 	private long startMillis;
+	
+	private String passengerDetails, flightDetails, queueJoin, 
+					deskJoin, checkedIn, flightClosed, mainLog = "";
 
 	private Logger() {
 	}
@@ -23,5 +30,79 @@ public class Logger {
 	public void resetTimer() {
 		startMillis = System.currentTimeMillis();
 	}
-
+	
+	private String GetTimeStamp()
+	{
+		return "[" + String.format("%07d",  (System.currentTimeMillis() - startMillis)) + "] ";
+	}
+	
+	public void LogPassengerDetails(Customer c)
+	{
+		passengerDetails += GetTimeStamp() + c.toString() + "\n";
+	}
+	
+	public void LogFlightDetails(Flight f)
+	{
+		flightDetails += GetTimeStamp() + f.toString() + "\n";
+	}
+	
+	public void PassengerJoinedQueue(Customer c)
+	{
+		String s = GetTimeStamp() + c.toString() + " has entered the queue\n";
+		queueJoin += s;
+		mainLog += s;
+	}
+	
+	public void PassengerMovedToDesk(Customer c, String deskName)
+	{
+		String s = GetTimeStamp() + c.toString() + " has moved to " + deskName + "\n";
+		deskJoin += s;
+		mainLog += s;
+	}
+	
+	public void PassengerCheckedIn(Customer c, Flight f, String deskName, float baggageFee)
+	{
+		String s = GetTimeStamp() + c.toString() + "has been checked in at " + deskName + ". Baggage Fee: " + baggageFee + "\n";
+		checkedIn += s;
+		mainLog += s;
+	}
+	
+	public void FlightClosed(Flight f)
+	{
+		String s = "Flight " + f.toString() + "is now closed";
+		flightClosed += s;
+		mainLog += s;
+	}
+	
+	public String GetSummary()
+	{
+		String sum = "Simulation Log:\n\n";
+		
+		// main log
+		sum += "Main Log:\n" + mainLog + "\n\n";
+		
+		// additional information
+		sum += "Additional Information:\n";
+		sum += "Queue Join:\n" + queueJoin + "\n";
+		sum += "Desk Login:\n" + deskJoin + "\n";
+		sum += "Check-In:\n" + checkedIn + "\n";
+		sum += "Flight Status:\n" + flightClosed + "\n";
+		
+		// flights and passengers
+		sum += "\nPassenger Details:\n" + passengerDetails + "\n";
+		sum += "Flight Details:\n" + flightDetails;
+		
+		return sum;
+	}
+	
+	public void WriteSummaryToFile(String filename)
+	{		
+		try 
+		{
+			BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+			writer.write(GetSummary());
+			writer.close();
+		}
+		catch (IOException e) { e.printStackTrace(); }
+	}
 }
