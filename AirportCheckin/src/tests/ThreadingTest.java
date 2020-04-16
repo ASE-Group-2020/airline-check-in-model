831b34a82cfc531d12e54c246fea3993a4577c8f
DRGladwell <david_gladwell@hotmail.com>
2020-04-16 18:17:32 +0100

    Added a nearly wroking version of ThreadingTest
    Changed allflights l.41 simulaor to public
    Changed customer l.9 flights to public

--- AirportCheckin/src/tests/ThreadingTest.java
+++ AirportCheckin/src/tests/ThreadingTest.java
@@ -1,43 +1,37 @@
 package tests;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 
-import airlineinterface.Customer;
 import airlineinterface.Desk;
-import airlineinterface.Flight;
 import airlineinterface.Logger;
 import airlineinterface.Simulator;
-import airlineinterface.WaitingQueue;
-import exceptions.InvalidValueException;
-
+import airlineinterface.Flight;
 /* This test unit will test a large chunk of code. The main purpose is to check if threading is working correctly. 
 *  However issues can arise if there are other mistakes in the code.
 */
 
+
+
 public class ThreadingTest {
 
+	public Simulator sim;
+	public int num;
 	
-
+	@BeforeEach
 	void setup(){
 		Logger.instance().resetTimer();									// Start logger
 		
-		Simulator sim = new Simulator(3);								// the argument is the number of desks instantiated 
+	    sim = new Simulator(3);								// the argument is the number of desks instantiated 
 
-		sim.readFlightsFromFile("dataFlight-40c.csv");
-		sim.readCustomersFromFile("dataCustomer-40c.csv");
+		sim.readFlightsFromFile("dataFlight-Testc.csv");
+		sim.readCustomersFromFile("dataCustomer-Testc.csv");
 
 		//sim.makeCustomersArrive(5);										// Delays the arrival of customers
 		
-		sim.start(1000, 120, false);										// (simSpeed, runTime, randomness)
+		sim.start(100000, 4, true);										// (simSpeed, runTime, randomness)
+		this.num = 12;
 	}
 
 	/*
@@ -73,18 +67,13 @@ public class ThreadingTest {
 	 * 
 	 */
 	void fiveDeskThreadTest() {
-		/*
-		 * Thread threadQueue1 = new Thread(q1); // Start threads threadQueue1.start();
-		 * Thread threadDesk1 = new Thread(d1); threadDesk1.start(); Thread threadDesk2
-		 * = new Thread(d2); threadDesk2.start(); Thread threadDesk3 = new Thread(d3);
-		 * threadDesk3.start(); Thread threadDesk4 = new Thread(d4);
-		 * threadDesk4.start(); Thread threadDesk5 = new Thread(d5);
-		 * threadDesk5.start();
-		 * 
-		 * System.out.println("London outbond flight number of passangers: "+
-		 * LON1.customers.size());
-		 * System.out.println("Paris outbound flight number of passangers: "+
-		 * PAR2.customers.size());
-		 */
+		//for(Desk d : sim.allDesks) {		// Add all flights to guiController
+		int num = 20;
+		for(Flight f : sim.allFlights) {
+			System.out.println("the number of people in flight: " + f.customers.size());
+			assertEquals(int(f.customers.size()),num,"The code has failed to correctly place the queing people into both flights.");
+		}
+		
 	}
+	
 }
